c749d3e7be8ae528a70508c7d87e691338774d57
Evan-J-Hahn <evan.hahn@me.com>
2020-06-23 18:40:16 -0700

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import MaterialTable from "material-table"
+import './App.css'
 
-function App() {
+const App = () => {
+  const { useState } = React;
+  const [selectedRow, setSelectedRow] = useState(null)
+
+  const [columns] = useState([
+    { title: 'First Name', field: 'firstName' },
+    { title: 'Last Name', field: 'lastName' },
+    { title: 'Age', field: 'age', type: 'numeric' },
+    { title: 'Location', field: 'location' }
+  ]);
+
+  const [data, setData] = useState([
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' },
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' },
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' },
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' },
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' },
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' },
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' },
+    { firstName: 'Roger', lastName: 'Clemente', age: 29, location: 'Corvallis' },
+    { firstName: 'Analisa', lastName: 'Cordova', age: 27, location: 'Los Angeles' }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ maxWidth: '100%' }}>
+      <MaterialTable
+        title="Users"
+        columns={columns}
+        data={data}
+        onRowClick={((event, selectedRow) => setSelectedRow(selectedRow.tableData.id))}
+        options={{
+          rowStyle: rowData => ({
+            backgroundColor: (selectedRow === rowData.tableData.id) ? '#EEE' : '#FFF'
+          })
+        }}
+        editable={{
+          onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                setData([...data, newData]);
+                
+                resolve();
+              }, 1000)
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataUpdate = [...data];
+                const index = oldData.tableData.id;
+                dataUpdate[index] = newData;
+                setData([...dataUpdate]);
+
+                resolve();
+              }, 1000)
+            }),
+          onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                const dataDelete = [...data];
+                const index = oldData.tableData.id;
+                dataDelete.splice(index, 1);
+                setData([...dataDelete]);
+                
+                resolve()
+              }, 1000)
+            }),
+        }}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
